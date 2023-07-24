6c668db6096f859f6d4eafe17c555d16b6a85995
FilipaMerino <filipaam@outlook.com>
2023-07-24 13:17:31 +0100

    details

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -11,34 +11,43 @@ export const metadata = {
 const AboutPage = () => {
   return (
     <div className="">
-      {/* THIS IS THE TITLE AND TEXT OF ABOUT SESSION */}
-      <div className=" p-10 lg:h-[20rem] flex flex-col justify-center mx-5 lg:mx-20 lg:p-5">
-        <div className="flex mb-2 lg:mb-5">
-          <h1 className="text-3xl lg:text-5xl">About</h1>
-          <h1 className="text-3xl lg:text-5xl text-[#fb653e] ml-3">Us.</h1>
+      <div className="flex items-center">
+        <div className="hidden lg:block  flex-col justify-center items-start lg:w-[20rem] lg:ml-20">
+          <p className="font-semibold	lg:mb-1">About Us.</p>
+          <p className="font-semibold	lg:mb-1">Our Values.</p>
+          <p className="font-semibold	">Team.</p>
         </div>
 
-        <hr className="horizontalLineCard block  lg:hidden"></hr>
+        <div className=" p-10 lg:h-[20rem] flex flex-col justify-center mx-5 lg:mx-20 lg:p-5">
+          <div className="flex mb-2 lg:mb-5">
+            <h1 className="text-3xl lg:text-5xl">About</h1>
+            <h1 className="text-3xl lg:text-5xl text-[#fb653e] ml-3">Us.</h1>
+          </div>
 
-        <p className="mb-5">
-          At Admin Virtuosa, our mission is to serve as a{" "}
-          <strong>support system</strong> for small business owners and
-          entrepreneurs to fulfill their objectives while advocating for a{" "}
-          <strong>balanced</strong> approach to work and life. We are proud to
-          offer exceptional personalized services to help prevent burnout and
-          enhance the mental health and well-being of our clients.
-        </p>
+          <hr className="horizontalLineCard block  lg:hidden"></hr>
 
-        <p>
-          Through understanding and collaboration, we work to create custom
-          business plans for our clients that take into consideration their
-          individual needs and goals. By combining our passion to serve with our
-          problem-solving skills, we help to build a process that ensures our
-          clients reach their personal and professional goals. We strive to
-          create a genuine partnership built on trust, respect and a shared
-          vision for growth and opportunity.
-        </p>
+          <p className="mb-5">
+            At Admin Virtuosa, our mission is to serve as a{" "}
+            <strong>support system</strong> for small business owners and
+            entrepreneurs to fulfill their objectives while advocating for a{" "}
+            <strong>balanced</strong> approach to work and life. We are proud to
+            offer exceptional personalized services to help prevent burnout and
+            enhance the mental health and well-being of our clients.
+          </p>
+
+          <p>
+            Through understanding and collaboration, we work to create custom
+            business plans for our clients that take into consideration their
+            individual needs and goals. By combining our passion to serve with
+            our problem-solving skills, we help to build a process that ensures
+            our clients reach their personal and professional goals. We strive
+            to create a genuine partnership built on trust, respect and a shared
+            vision for growth and opportunity.
+          </p>
+        </div>
       </div>
+
+      {/* THIS IS THE TITLE AND TEXT OF ABOUT SESSION */}
 
       {/* THIS IS THE BANNER FROM THE ABOUT SESSION */}
       <div className="flex items-center relative h-[20rem]">
@@ -57,6 +66,17 @@ const AboutPage = () => {
           <p className="text-sm lg:text-lg">Brian Tracy</p>
         </div>
       </div>
+
+      {/* HERE IS THE VALUES */}
+
+<div>
+
+
+  
+</div>
+
+
+
     </div>
   );
 };
--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -13,7 +13,7 @@ const Banner = () => {
             <h1 className="text-3xl md:text-5xl text-white">Business</h1>
           <div className="md:flex">
             <h1 className="text-3xl md:text-5xl text-white">Management</h1>
-            <h1 className="text-3xl md:text-5xl text-white md:mb-2">Solutions</h1>
+            <h1 className="text-3xl md:text-5xl text-white md:mb-2 md:ml-2" >Solutions</h1>
 
           </div>
           <h3 className="text-white text-lg md:text-3xl mb-5 mt-2">Simplify. Organize. Succeed.</h3>
