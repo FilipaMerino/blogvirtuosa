4cb35f25a71780409e43356095db3c3e8f038250
FilipaMerino <filipaam@outlook.com>
2023-07-24 12:13:25 +0100

    about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,11 +10,11 @@ export const metadata = {
 
 const AboutPage = () => {
   return (
-    <div className="mx-20 p-5 h-[20rem] flex flex-col justify-center">
-      <div className="mb-10">
+    <div className="mx-20 p-5  ">
+      <div className=" h-[20rem] flex flex-col justify-center">
         <div className="flex mb-5">
-          <h1 className="text 3xl lg:text-5xl">About</h1>
-          <h1 className="text 3xl lg:text-5xl text-[#fb653e] ml-3">Us.</h1>
+          <h1 className="text-3xl lg:text-5xl">About</h1>
+          <h1 className="text-3xl lg:text-5xl text-[#fb653e] ml-3">Us.</h1>
         </div>
 
         <p>
@@ -25,14 +25,31 @@ const AboutPage = () => {
           offer exceptional personalized services to help prevent burnout and
           enhance the mental health and well-being of our clients.
         </p>
+
+        <p>
+          Through understanding and collaboration, we work to create custom
+          business plans for our clients that take into consideration their
+          individual needs and goals. By combining our passion to serve with our
+          problem-solving skills, we help to build a process that ensures our
+          clients reach their personal and professional goals. We strive to
+          create a genuine partnership built on trust, respect and a shared
+          vision for growth and opportunity.
+        </p>
       </div>
 
-      <div className="flex items-center relative h-[35rem]">
-      <Image src={office} layout="fill" objectFit="cover" alt="banner"></Image>
-
-</div>
+      <div className="flex items-center relative h-[15rem]">
+        <Image
+          src={office}
+          layout="fill"
+          objectFit="cover"
+          alt="banner"></Image>
+      </div>
     </div>
   );
 };
 
 export default AboutPage;
+
+// This is a good quote to have on a banner on this page, with the opacity bigger
+// “ Success is the ability to live your life the way you want to live it, doing what you most enjoy, surrounded by people who you admire and respect. ”
+// Brian Tracy
