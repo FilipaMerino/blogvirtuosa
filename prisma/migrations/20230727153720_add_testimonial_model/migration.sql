acc3d09c2b8fbdba3e1df42c48855eb2b222f613
FilipaMerino <filipaam@outlook.com>
2023-07-27 16:39:02 +0100

    add testimonial model

---
+++ prisma/migrations/20230727153720_add_testimonial_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Testimonial" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "avatar" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "companyName" TEXT NOT NULL,
+    "testimonialText" TEXT NOT NULL
+);
