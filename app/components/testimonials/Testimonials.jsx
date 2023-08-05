a4982a8f26279608569c4675178f3632a2287e9d
FilipaMerino <filipaam@outlook.com>
2023-08-05 16:50:10 +0100

    changes in testimonials design

--- app/components/testimonials/Testimonials.jsx
+++ app/components/testimonials/Testimonials.jsx
@@ -7,51 +7,34 @@ import { UserContext } from "../../contexts/UserContext";
 import { BsTrash3Fill } from "react-icons/bs";
 
 const Testimonials = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-
-
-
-  const handlePrevious = () => {
-    // Find the index of the current testimonial ID in the array
-    const currentIndex = testimonials.findIndex((testimonial) => testimonial.id === currentSlide);
-
-    // Find the previous testimonial ID based on the current index
-    const previousTestId = testimonials[
-      (currentIndex - 1 + testimonials.length) % testimonials.length
-    ].id;
-
-    // Update the currentSlide state with the previous testimonial ID
-    setCurrentSlide(previousTestId);
-  };
-
-  const handleNext = () => {
-    // Find the index of the current testimonial ID in the array
-    const currentIndex = testimonials.findIndex((testimonial) => testimonial.id === currentSlide);
-
-    // Find the next testimonial ID based on the current index
-    const nextTestId = testimonials[(currentIndex + 1) % testimonials.length].id;
-
-    // Update the currentSlide state with the next testimonial ID
-    setCurrentSlide(nextTestId);
-  };
-
-
-
-
   const { currentUser } = useContext(UserContext);
-
   const [testimonials, setTestimonials] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const fetchTestimonials = async () => {
     const res = await fetch("/api/testimonials");
-    const testimonials = await res.json();
+    const testimonialsData = await res.json();
 
-    setTestimonials([...testimonials]);
+    setTestimonials(testimonialsData);
   };
 
   useEffect(() => {
     fetchTestimonials();
   }, []);
+
+  const handlePrevious = () => {
+    if (testimonials.length === 0) return;
+
+    setCurrentSlide(
+      (currentSlide - 1 + testimonials.length) % testimonials.length
+    );
+  };
+
+  const handleNext = () => {
+    if (testimonials.length === 0) return;
+
+    setCurrentSlide((currentSlide + 1) % testimonials.length);
+  };
 
   const deleteTestimonial = async (id) => {
     const res = await fetch(`/api/testimonials/${id}`, {
@@ -62,72 +45,82 @@ const Testimonials = () => {
   };
 
   return (
-
     <div className="lg:h-[40rem] flex justify-center">
       <div className="flex items-center mx-20">
-
-
-        <div className="w-[50%] mr-20">
+        <div className="w-[40%] mr-20">
           {/* AQUI VAI ESTAR A CENA COM O TESTIMONIAL A OCUPAR METADE */}
           <h1 className="text-3xl lg:text-5xl mb-3">Testimonials</h1>
           <hr className="horizontalLineWebDev my-5"></hr>
 
-          <p className="text-md md:text-lg">
-            Our customers' happiness is our top priority, and we're humbled by
-            the kind words they share.
+          <p className="text-md md:text-lg mb-3">
+            Our customers are at the heart of everything we do. Their
+            satisfaction isn't just a goal, it's our measure of success. Here's
+            what some of our valued customers have to say about their
+            experiences with us.
           </p>
+
+
+
+
+
+          <AddTestimonial refreshTestimonials={fetchTestimonials}/>
         </div>
 
-
-
-
-
-          {/* Aqui vai estar o testimonial */}
-          <div className="w-[55%] flex ">
-            <div className="carousel ">
-              {testimonials.map((testimonial) => (
-                <div className="carousel-item relative w-full flex flex-col">
-                  <div className="flex justify-center">
-                    <img src={testimonial.avatar} className="w-32"></img>
-                  </div>
-
-                  <div className="flex flex-col items-center">
-                    <h4 className="text-xl md:text-2xl font-semibold mb-1">
-                      {testimonial.name}
-                    </h4>
-                    <p className="text-xs md:text-sm italic mb-5">
-                      {testimonial.companyName}
-                    </p>
-                    <p className="text-md italic mx-20 text-center">
-                      {testimonial.testimonialText}
-                    </p>
-                  </div>
-
-                  <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 ">
-                    <button
-                      onClick={handlePrevious}
-                      className="btn btn-circle bg-[#f2f2f2]  text-[#333333] hover:bg-[#e0e0e0] border-none" >
-                      ❮
-                    </button>
-                    <button
-                      onClick={handleNext}
-                      className="btn btn-circle bg-[#f2f2f2]  text-[#333333] hover:bg-[#e0e0e0] border-none">
-                      ❯
-                    </button>
-                  </div>
-
-
-
+        {/* Aqui vai estar o testimonial */}
+        <div className="w-[55%] flex ">
+          <div className="carousel ">
+            {testimonials.map((testimonial, index) => (
+              <div
+                key={testimonial.id}
+                className={`carousel-item relative w-full flex flex-col ${
+                  index === currentSlide ? "visible" : "hidden"
+                }`}>
+                <div className="flex justify-center">
+                  <img
+                    src={testimonial.avatar}
+                    className="w-32"
+                    alt="testimonial"></img>
                 </div>
-              ))}
-            </div>
+
+                <div className="flex flex-col items-center">
+                  <h4 className="text-xl md:text-2xl font-semibold mb-1">
+                    {testimonial.name}
+                  </h4>
+
+                  <p className="text-xs md:text-sm italic mb-5">
+                    {testimonial.companyName}
+                  </p>
+                  <p className="text-md italic mx-20 text-center">
+                    {testimonial.testimonialText}
+                  </p>
+
+                  {currentUser && (
+                    <button
+                      className="btn mt-10"
+                      onClick={() => deleteTestimonial(testimonial.id)}>
+                      <BsTrash3Fill className="hover:text-[#fb653e]" />
+                      Delete Testimonial
+                    </button>
+                  )}
+                </div>
+
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 ">
+                  <button
+                    onClick={handlePrevious}
+                    className="btn btn-circle bg-[#f2f2f2]  text-[#333333] hover:bg-[#e0e0e0] border-none">
+                    ❮
+                  </button>
+                  <button
+                    onClick={handleNext}
+                    className="btn btn-circle bg-[#f2f2f2]  text-[#333333] hover:bg-[#e0e0e0] border-none">
+                    ❯
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-
-
-
-
-
         </div>
+      </div>
     </div>
   );
 };
